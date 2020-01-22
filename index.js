c398f3b6e5a315e62203ec4c720fe9a94a425422
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-01-21 20:31:24 -0500

    Finnished with Task 1.

--- index.js
+++ index.js
@@ -39,8 +39,29 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+  
+}
 
+Person.prototype.eat = function(someFood){
+  if(this.stomach.length < 10){
+     this.stomach.push(someFood);
+  }
+  return;
+  
+  
+}
+
+Person.prototype.poop = function(someFood){
+  return this.stomach =[];
+  
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
 }
 
 /*
@@ -57,7 +78,9 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  // this.model = 0;
+  // this.odometer = 0; 
 
 }
 
